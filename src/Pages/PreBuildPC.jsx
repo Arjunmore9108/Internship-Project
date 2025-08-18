fb148cc8a5b3ba7f25f786336debd8d669d70dca
Arjunmore9108 <arjunmore9108@gmail.com>
2025-08-18 12:53:55 +0530

    Desktop icon added

--- src/Pages/PreBuildPC.jsx
+++ src/Pages/PreBuildPC.jsx
@@ -24,7 +24,7 @@ function PCPeripherals() {
                         </Link>
                         <Link to="/Home/PreBuildPC/Desktops" style={{textDecoration:"None"}}>
                         <div className="Options">
-                            <img className="Option" src="OutputDevices.jpg" />
+                            <img className="Option" src="Desktop.png" />
                             <p>DESKTOPS</p>
                         </div>
                         </Link>
